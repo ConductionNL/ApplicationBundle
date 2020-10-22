7c3e405fb1850d0076279dbd5225485281d2f5a1
Ruben van der Linde <ruben@conduction.nl>
2020-10-22 08:29:32 +0200

    Basic Bundle setup

---
+++ ApplicationBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+// Conduction/ApplicationBundle/ApplicationBundle.php
+
+/*
+ * This file is part of the Conduction Application
+ *
+ * (c) Conduction <info@conduction.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Conduction\ApplicationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ApplicationBundle extends Bundle
+{
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ApplicationBundle
-This bundle provides the an example application on symfony for the commonground ecosystem
+# LandKitBundle
+This bundle holds the landkit template symfony
